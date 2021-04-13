95dfcdc86d3a93a756ac09d71d08721bacb0eebc
xyba1337 <62798507+xyba1337@users.noreply.github.com>
2021-04-13 22:51:06 +0200

    Add files via upload

---
+++ BootstrappingTut/NamedPipes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace bootstrapping
+{
+    class NamedPipes
+    {
+        public static string luapipename = ("YnJ1aA==");//easyexploits api pipename
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool WaitNamedPipe(string name, int timeout);
+        public static bool NamedPipeExist(string pipeName)
+        {
+            bool result;
+            try
+            {
+                int timeout = 0;
+                if (!WaitNamedPipe(Path.GetFullPath(string.Format("\\\\\\\\.\\\\pipe\\\\{0}", pipeName)), timeout))
+                {
+                    int lastWin32Error = Marshal.GetLastWin32Error();
+                    if (lastWin32Error == 0)
+                    {
+                        result = false;
+                        return result;
+                    }
+                    if (lastWin32Error == 2)
+                    {
+                        result = false;
+                        return result;
+                    }
+                }
+                result = true;
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
---
+++ BootstrappingTut/Program.cs
@@ -0,0 +1,111 @@
+﻿using bootstrapping;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Threading;
+
+namespace BootstrappingTut
+{
+    class Program
+    {
+        //create if pipe is connected (X)
+        //delete files which might cause some problems (X)
+        //download the file (X)
+        //extract the file (X)
+        //open the file (X)
+
+
+        public static void removeproblems()
+        {
+            string dc = @".\Bin";
+            if (Directory.Exists(dc))
+            try
+            {
+                Directory.Delete(dc, true);
+
+                Thread.Sleep(1700);
+            }
+            catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Something went wrong " + ex.ToString());
+                }
+            Console.WriteLine("Remove Problems done");
+        }
+        public static void check()
+        {
+            for(;;)
+            {
+                if (NamedPipes.NamedPipeExist(NamedPipes.luapipename))
+                {
+                    Console.WriteLine("works");
+                }
+                else
+                {
+                    Console.WriteLine("ah shit");
+                }
+                //foreach (Process pc in Process.GetProcessesByName("RobloxPlayerBeta"))
+                //    try
+                //    {
+                //        pc.Kill();
+                //    }
+                //    catch (Exception ex)
+                //    {
+                //        Console.WriteLine($"Nothing to worrie about " + ex.ToString());
+                //    }
+                Thread.Sleep(2000);
+            }
+            
+        }
+
+        public static void download()
+        {
+            WebClient wc = new WebClient();
+            wc.DownloadFileAsync(new Uri ("https://cdn.discordapp.com/attachments/487021226519298050/720271531770642533/files.zip"), @".\files.zip");
+            Thread.Sleep(2500);
+            Console.WriteLine("Downloaded files");
+        }
+
+        public static void extract()
+        {
+            string zippath = @".\files.zip";
+            ZipFile.ExtractToDirectory(zippath, @".\Bin");
+            Thread.Sleep(1000);
+
+            string zip = @".\files.zip";
+            if (File.Exists(zip))
+                try
+                {
+                    File.Delete(zip);
+
+                    Thread.Sleep(1700);
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Something went wrong " + ex.ToString());
+                }
+            Console.WriteLine("Exploit Extracted");
+        }
+
+        public static void startprocess()
+        {
+            string executeablepath = @".\Bin\TestExploit.exe";
+            Process.Start(executeablepath);
+            Thread.Sleep(600);
+            Console.WriteLine("Starting process");
+        }
+        static void Main(string[] args)
+        {
+            removeproblems();
+            download();
+            extract();
+            startprocess();
+            Console.WriteLine("");
+            Console.WriteLine("Window won't close due to the fact that you need to tell the problem to close itself after everything is done. You can find out how to do that yourself im pretty sure! Good luck :D");
+            Console.ReadKey();
+        }
+    }
+}
